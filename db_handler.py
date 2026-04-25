3bb09a67d89df2b8bf5d5a398b5e0d78ecf9799e
aidan <aidanboudreau@ufl.edu>
2026-04-24 20:37:48 -0400

    grant extension complete

--- db_handler.py
+++ db_handler.py
@@ -41,49 +41,49 @@ def add_customer(new_customer: Customer = None):
     new_customer - A Customer object containing a new customer to be inserted into the DB in the customer table.
         new_customer and its attributes will never be None.
     """
-    c_customer_sk = 1 #table is empty
+    customer_sk = 1 #table is empty
     
     # get max c_sk and add 1 for c_customer_sk
     cur.execute("SELECT MAX(c_customer_sk) FROM Customer")
     for row in cur:
         if row[0] is not None:
-            c_customer_sk = row[0] + 1
+            customer_sk = row[0] + 1
 
-    ca_address_sk = 1 #table is empty
+    address_sk = 1 #table is empty
 
     # get max c_sk and add 1 for ca_address_sk
     cur.execute("SELECT MAX(ca_address_sk) FROM customer_address")
     for row in cur:
         if row[0] is not None:
-            ca_address_sk = row[0] + 1
+            address_sk = row[0] + 1
 
     # get customer first & last name
     first_last = new_customer.name.split(" ", 1)
-    c_first_name = first_last[0]
+    first_name = first_last[0]
     if len(first_last)>1:
-        c_last_name = first_last[1]
+        last_name = first_last[1]
     else:
-        c_last_name = ""
+        last_name = ""
 
     # tokenize address
     tokens = new_customer.address.split(", ")
     street = tokens[0].split(" ", 1)
-    ca_street_number = street[0]
+    street_num = street[0]
     if len(street)>1:
-        ca_street_name = street[1]
+        street_name = street[1]
     else:
-        ca_street_name = ""
-    ca_city = tokens[1]
+        street_name = ""
+    city = tokens[1]
     state_zip = tokens[2].split(" ")
-    ca_state = state_zip[0]
-    ca_zip = state_zip[1]
+    state = state_zip[0]
+    zip = state_zip[1]
 
     query = "INSERT INTO Customer VALUES (?, ?, ?, ?, ?, ?)"
-    params = (c_customer_sk, new_customer.customer_id, c_first_name, c_last_name, new_customer.email, ca_address_sk)
+    params = (customer_sk, new_customer.customer_id, first_name, last_name, new_customer.email, address_sk)
     cur.execute(query, params)
 
     query = "INSERT INTO customer_address VALUES (?, ?, ?, ?, ?, ?)"
-    params = (ca_address_sk, ca_street_number, ca_street_name, ca_city, ca_state, ca_zip)
+    params = (address_sk, street_num, street_name, city, state, zip)
     cur.execute(query, params)
 
 def edit_customer(original_customer_id: str = None, new_customer: Customer = None):
@@ -102,7 +102,7 @@ def edit_customer(original_customer_id: str = None, new_customer: Customer = Non
         params_customer_changes.append(new_customer.customer_id)
 
     # changing customer name
-    if new_customer.name is not None:\
+    if new_customer.name is not None:
         # tokenize name, append name change and params respectively
         first_last = new_customer.name.split(" ", 1)
         first_name = first_last[0]
@@ -188,8 +188,18 @@ def grant_extension(item_id: str = None, customer_id: str = None):
     """
     Adds 14 days to the due_date.
     """
-    raise NotImplementedError("you must implement this function")
-
+    # set new due date from adding 14 days to old due date
+    query = "SELECT due_date FROM rental WHERE item_id = ? AND customer_id = ?"
+    params = (item_id, customer_id)
+    cur.execute(query, params)
+    
+    for row in cur:
+        new_due_date = row[0] + timedelta(days=14)
+    
+    # UPDATE rental to new due date
+    query = "UPDATE rental SET due_date = ? WHERE item_id = ? AND customer_id = ?"
+    params = (new_due_date, item_id, customer_id)
+    cur.execute(query, params)
 
 def get_filtered_items(filter_attributes: Item = None,
                        use_patterns: bool = False,
@@ -227,20 +237,23 @@ def get_filtered_customers(filter_attributes: Customer = None, use_patterns: boo
     """
     Returns a list of Customer objects matching the filters.
     """
+    # join customer on customer address to get all info in one row
     query = "SELECT Customer.c_customer_id, Customer.c_first_name, Customer.c_last_name, customer_address.ca_street_number, customer_address.ca_street_name, customer_address.ca_city, customer_address.ca_state, customer_address.ca_zip, Customer.c_email_address From Customer" \
     " JOIN customer_address ON Customer.c_current_addr_sk = customer_address.ca_address_sk"
     params = []
 
+    # get customer data from customer id
     if filter_attributes is not None and filter_attributes.customer_id is not None:
         query = query + " WHERE Customer.c_customer_id = ?"
         params.append(filter_attributes.customer_id) 
 
     cur.execute(query, params)
 
+    # create Customer (Data Model) from executed query. create name and address from tokens.
     customers=[]
     for row in cur:
         name = row[1] + " " + row[2]
-        address = row[3] + " " + row[4] + ", " + row[5] + ", " + row[6] + ", " + row[7]
+        address = row[3] + " " + row[4] + ", " + row[5] + ", " + row[6] + " " + row[7]
         customers.append(Customer(customer_id=row[0], name=name, address=address, email=row[8]))
     return customers
 
@@ -253,7 +266,34 @@ def get_filtered_rentals(filter_attributes: Rental = None,
     """
     Returns a list of Rental objects matching the filters.
     """
-    raise NotImplementedError("you must implement this function")
+    # get attributes from rental
+    query = "SELECT * FROM rental"
+    params = []
+
+    # append conditions and params by the requested item and customer ids
+    if filter_attributes is not None:
+        conditions = []
+        if filter_attributes.item_id is not None:
+            conditions.append("item_id = ?")
+            params.append(filter_attributes.item_id)
+        if filter_attributes.customer_id is not None:
+            conditions.append("customer_id = ?")
+            params.append(filter_attributes.customer_id)
+
+        # add " WHERE item_id = i_id AND customer_id = c_id"
+        if conditions:
+            query = query + " WHERE "
+            for i in range(len(conditions)-1):
+                query = query + conditions[i] + " AND "
+            query = query + conditions[-1]
+    
+    cur.execute(query, params)
+
+    # create Rental (Data Model) from query, return to helper_functions.py
+    rentals=[]
+    for row in cur:
+        rentals.append(Rental(item_id=row[0], customer_id=row[1], rental_date=str(row[2]), due_date=str(row[3])))
+    return rentals
 
 
 def get_filtered_rental_histories(filter_attributes: RentalHistory = None,
